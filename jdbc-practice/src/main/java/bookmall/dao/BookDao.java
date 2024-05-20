050df38fd7491a59b4100144a0df23337ba49ecd
BbingDdang <ddaa1541@gmail.com>
2024-05-20 15:00:23 +0900

    init file setting bookmall

---
+++ jdbc-practice/src/main/java/bookmall/dao/BookDao.java
@@ -0,0 +1,13 @@
+package bookmall.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import bookmall.vo.BookVo;
+
+public class BookDao {
+	//url = "jdbc:mariadbL//192.168.64.10
+}
